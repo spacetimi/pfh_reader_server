bfa9626225489ad08970db4b1f5ac0e3ce2c1272
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-24 22:44:13 +0530

    Move activity_overview into its own file under parsers/common

---
+++ app_src/parser/parsers/common/activity_overview.go
@@ -0,0 +1,88 @@
+package common
+
+import (
+	"strconv"
+
+	"github.com/spacetimi/pfh_reader_server/app_src/app_core"
+	"github.com/spacetimi/pfh_reader_server/app_src/parser/parsers/parser_metadata"
+	"github.com/spacetimi/timi_shared_server/utils/logger"
+	"github.com/spacetimi/timi_shared_server/utils/time_utils"
+)
+
+type ActivityOverviewData struct {
+	ActivityPeriods [parser_metadata.NUM_ACTIVITY_PERIODS_PER_DAY]*ActivityPeriodData
+}
+
+type ActivityPeriodData struct {
+	SecondsInCategory map[app_core.Category_t]int64
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func NewActivityOverviewData() *ActivityOverviewData {
+
+	aod := &ActivityOverviewData{}
+
+	for i := 0; i < len(aod.ActivityPeriods); i = i + 1 {
+		aod.ActivityPeriods[i] = NewActivityPeriodData()
+	}
+
+	return aod
+}
+
+func NewActivityPeriodData() *ActivityPeriodData {
+	apd := &ActivityPeriodData{
+		SecondsInCategory: make(map[app_core.Category_t]int64),
+	}
+
+	for i := app_core.CATEGORY_PRODUCTIVE; i <= app_core.CATEGORY_UNCLASSIFIED; i = i + 1 {
+		apd.SecondsInCategory[i] = 0
+	}
+
+	return apd
+}
+
+func (aod *ActivityOverviewData) AddActivity(category app_core.Category_t, timestampOfActivity int64, seconds int64) {
+	timeOfActivity := time_utils.UnixTimeStampToLocalTime(timestampOfActivity)
+	beginningOfDay := time_utils.BeginningOfDay(timeOfActivity)
+
+	secondsIntoDay := time_utils.DurationToSeconds(time_utils.GetDurationBetweenTimes(beginningOfDay, timeOfActivity))
+	periodIndex := int(secondsIntoDay / parser_metadata.ACTIVITY_PERIOD_LENGTH_SECONDS)
+
+	if periodIndex < 0 || periodIndex >= len(aod.ActivityPeriods) {
+		logger.LogError("period index out of range" +
+			"|period index=" + strconv.Itoa(periodIndex) +
+			"|timestamp of activity=" + strconv.FormatInt(timestampOfActivity, 10))
+		return
+	}
+	activityPeriod := aod.ActivityPeriods[periodIndex]
+	activityPeriod.AddActivity(category, seconds)
+}
+
+func (apd *ActivityPeriodData) AddActivity(category app_core.Category_t, seconds int64) {
+	secondsInCategory, ok := apd.SecondsInCategory[category]
+	if !ok {
+		secondsInCategory = 0
+		apd.SecondsInCategory[category] = secondsInCategory
+	}
+
+	apd.SecondsInCategory[category] = secondsInCategory + seconds
+}
+
+func (apd *ActivityPeriodData) GetSecondsInCategory(category app_core.Category_t) int64 {
+	secondsInCategory, ok := apd.SecondsInCategory[category]
+	if !ok {
+		return 0
+	}
+	return secondsInCategory
+}
+
+func (aod *ActivityOverviewData) GetActivityInPeriodsForCategory(category app_core.Category_t) []int64 {
+	result := make([]int64, len(aod.ActivityPeriods))
+
+	for i, apd := range aod.ActivityPeriods {
+		result[i] = apd.GetSecondsInCategory(category)
+	}
+
+	return result
+}
--- app_src/parser/parsers/day_overview_parser/day_overview_data.go
+++ app_src/parser/parsers/day_overview_parser/day_overview_data.go
@@ -1,18 +1,14 @@
 package day_overview_parser
 
 import (
-	"strconv"
-
 	"github.com/spacetimi/pfh_reader_server/app_src/app_core"
-	"github.com/spacetimi/pfh_reader_server/app_src/parser/parsers/parser_metadata"
-	"github.com/spacetimi/timi_shared_server/utils/logger"
-	"github.com/spacetimi/timi_shared_server/utils/time_utils"
+	"github.com/spacetimi/pfh_reader_server/app_src/parser/parsers/common"
 )
 
 type DayOverviewData struct {
 	CategoryOverviewsByCategory map[app_core.Category_t]*CategoryOverviewData
 	TotalTimeSeconds            int64
-	ActivityOverview            *ActivityOverviewData
+	ActivityOverview            *common.ActivityOverviewData
 }
 
 type CategoryOverviewData struct {
@@ -26,21 +22,13 @@ type AppUsageOverviewData struct {
 	TotalTimeSeconds int64
 }
 
-type ActivityOverviewData struct {
-	ActivityPeriods [parser_metadata.NUM_ACTIVITY_PERIODS_PER_DAY]*ActivityPeriodData
-}
-
-type ActivityPeriodData struct {
-	SecondsInCategory map[app_core.Category_t]int64
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 
 func NewDayOverviewData() *DayOverviewData {
 	return &DayOverviewData{
 		CategoryOverviewsByCategory: make(map[app_core.Category_t]*CategoryOverviewData),
 		TotalTimeSeconds:            0,
-		ActivityOverview:            newActivityOverviewData(),
+		ActivityOverview:            common.NewActivityOverviewData(),
 	}
 }
 
@@ -62,13 +50,13 @@ func (dod *DayOverviewData) AddAppUsageSecondsInCategory(category app_core.Categ
 	}
 	cod.addAppUsageInSeconds(appName, seconds)
 
-	dod.ActivityOverview.addActivity(category, timestamp, seconds)
+	dod.ActivityOverview.AddActivity(category, timestamp, seconds)
 
 	dod.TotalTimeSeconds += seconds
 }
 
 func (dod *DayOverviewData) GetActivityInPeriodsForCategory(category app_core.Category_t) []int64 {
-	return dod.ActivityOverview.getActivityInPeriodsForCategory(category)
+	return dod.ActivityOverview.GetActivityInPeriodsForCategory(category)
 }
 
 func (dod *DayOverviewData) GetAppsUsageSeconds() map[string]int64 {
@@ -121,74 +109,4 @@ func newAppUsageOverviewData(appName string) *AppUsageOverviewData {
 		AppName:          appName,
 		TotalTimeSeconds: 0,
 	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-func newActivityOverviewData() *ActivityOverviewData {
-
-	aod := &ActivityOverviewData{}
-
-	for i := 0; i < len(aod.ActivityPeriods); i = i + 1 {
-		aod.ActivityPeriods[i] = newActivityPeriodData()
-	}
-
-	return aod
-}
-
-func newActivityPeriodData() *ActivityPeriodData {
-	apd := &ActivityPeriodData{
-		SecondsInCategory: make(map[app_core.Category_t]int64),
-	}
-
-	for i := app_core.CATEGORY_PRODUCTIVE; i <= app_core.CATEGORY_UNCLASSIFIED; i = i + 1 {
-		apd.SecondsInCategory[i] = 0
-	}
-
-	return apd
-}
-
-func (aod *ActivityOverviewData) addActivity(category app_core.Category_t, timestampOfActivity int64, seconds int64) {
-	timeOfActivity := time_utils.UnixTimeStampToLocalTime(timestampOfActivity)
-	beginningOfDay := time_utils.BeginningOfDay(timeOfActivity)
-
-	secondsIntoDay := time_utils.DurationToSeconds(time_utils.GetDurationBetweenTimes(beginningOfDay, timeOfActivity))
-	periodIndex := int(secondsIntoDay / parser_metadata.ACTIVITY_PERIOD_LENGTH_SECONDS)
-
-	if periodIndex < 0 || periodIndex >= len(aod.ActivityPeriods) {
-		logger.LogError("period index out of range" +
-			"|period index=" + strconv.Itoa(periodIndex) +
-			"|timestamp of activity=" + strconv.FormatInt(timestampOfActivity, 10))
-		return
-	}
-	activityPeriod := aod.ActivityPeriods[periodIndex]
-	activityPeriod.addActivity(category, seconds)
-}
-
-func (apd *ActivityPeriodData) addActivity(category app_core.Category_t, seconds int64) {
-	secondsInCategory, ok := apd.SecondsInCategory[category]
-	if !ok {
-		secondsInCategory = 0
-		apd.SecondsInCategory[category] = secondsInCategory
-	}
-
-	apd.SecondsInCategory[category] = secondsInCategory + seconds
-}
-
-func (apd *ActivityPeriodData) getSecondsInCategory(category app_core.Category_t) int64 {
-	secondsInCategory, ok := apd.SecondsInCategory[category]
-	if !ok {
-		return 0
-	}
-	return secondsInCategory
-}
-
-func (aod *ActivityOverviewData) getActivityInPeriodsForCategory(category app_core.Category_t) []int64 {
-	result := make([]int64, len(aod.ActivityPeriods))
-
-	for i, apd := range aod.ActivityPeriods {
-		result[i] = apd.getSecondsInCategory(category)
-	}
-
-	return result
 }
