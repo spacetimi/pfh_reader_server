1934b8f978563ddb3b64f20d9d49950a50623d8e
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-28 21:44:24 +0530

    Add week to string conversions in week-view

--- app_src/app_route_handlers/home/week_view_helpers.go
+++ app_src/app_route_handlers/home/week_view_helpers.go
@@ -1,10 +1,12 @@
 package home
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/spacetimi/pfh_reader_server/app_src/parser/parsers/common"
 	"github.com/spacetimi/timi_shared_server/utils/file_utils"
+	"github.com/spacetimi/timi_shared_server/utils/logger"
 )
 
 func (hh *HomeHandler) getWeekviewPageObject(postArgs *parsedPostArgs) *WeekviewData {
@@ -14,18 +16,41 @@ func (hh *HomeHandler) getWeekviewPageObject(postArgs *parsedPostArgs) *Weekview
 	weekIdentifier := getWeekIdentifierFromWeekIndex(postArgs.CurrentWeekIndex)
 	dataFilePath := common.GetWeekDataFilePath(weekIdentifier)
 
+	currentWeekString := getCurrentWeekStringFromWeekIndex(postArgs.CurrentWeekIndex)
+	prevWeekIndex := postArgs.CurrentWeekIndex - 1
+	nextWeekIndex := postArgs.CurrentWeekIndex + 1
+	canShowNextWeekButton := postArgs.CurrentWeekIndex < 0
+	canShowPrevWeekButton := true // TODO: Add clamp on max prev-weeks data to save?
+
 	if !file_utils.DoesFileOrDirectoryExist(dataFilePath) {
 		weekviewPageObject = &WeekviewData{
 			ErrorablePage: ErrorablePage{
 				HasError:    true,
 				ErrorString: "No data for week #placeholder#",
 			},
-			CurrentWeekString: "placeholder",
+			CurrentWeekString:  currentWeekString,
+			PrevWeekIndex:      prevWeekIndex,
+			NextWeekIndex:      nextWeekIndex,
+			ShowNextWeekButton: canShowNextWeekButton,
+			ShowPrevWeekButton: canShowPrevWeekButton,
 		}
 		return weekviewPageObject
 	}
 
-	return &WeekviewData{}
+	weekviewPageObject = &WeekviewData{
+		ErrorablePage: ErrorablePage{
+			HasError:    false,
+			ErrorString: "",
+		},
+
+		CurrentWeekString:  currentWeekString,
+		PrevWeekIndex:      prevWeekIndex,
+		NextWeekIndex:      nextWeekIndex,
+		ShowNextWeekButton: canShowNextWeekButton,
+		ShowPrevWeekButton: canShowPrevWeekButton,
+	}
+
+	return weekviewPageObject
 }
 
 /*
@@ -34,13 +59,33 @@ func (hh *HomeHandler) getWeekviewPageObject(postArgs *parsedPostArgs) *Weekview
 */
 func getWeekIdentifierFromWeekIndex(weekIndex int) common.WeekIdentifier {
 	t := time.Now()
-	offsetWeeks := -1 * time.Duration(weekIndex) * 3600 * 24 * 7 * time.Second
-
-	t.Add(offsetWeeks)
+	t = t.AddDate(0, 0, 7*weekIndex)
 
 	yearNumber, weekNumber := t.ISOWeek()
 	return common.WeekIdentifier{
 		WeekNumber: weekNumber,
 		YearNumber: yearNumber,
 	}
+}
+
+func getCurrentWeekStringFromWeekIndex(weekIndex int) string {
+	if weekIndex > 0 {
+		logger.LogError("invalid week index" + "|week index=" + strconv.Itoa(weekIndex))
+		return "invalid"
+	}
+
+	if weekIndex == 0 {
+		return "This Week"
+	}
+
+	t := time.Now().AddDate(0, 0, 7*weekIndex)
+	t = getClosestPreviousMonday(t)
+
+	return "Week of " + t.Month().String() + " " + strconv.Itoa(t.Day())
+}
+
+func getClosestPreviousMonday(t time.Time) time.Time {
+	daysSinceMonday := (int(t.Weekday()-time.Monday) + 7) % 7
+	t = t.AddDate(0, 0, -1*daysSinceMonday)
+	return t
 }
