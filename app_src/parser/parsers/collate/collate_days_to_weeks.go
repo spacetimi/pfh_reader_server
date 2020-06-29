63731cfa504212759a223e38a98a4f1edd56d629
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-29 23:23:43 +0530

    Delete old day-data files to save space and speed up processing

--- app_src/parser/parsers/collate/collate_days_to_weeks.go
+++ app_src/parser/parsers/collate/collate_days_to_weeks.go
@@ -2,6 +2,7 @@ package collate
 
 import (
 	"errors"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -12,6 +13,7 @@ import (
 	"github.com/spacetimi/pfh_reader_server/app_src/user_preferences"
 	"github.com/spacetimi/timi_shared_server/utils/file_utils"
 	"github.com/spacetimi/timi_shared_server/utils/logger"
+	"github.com/spacetimi/timi_shared_server/utils/time_utils"
 )
 
 func CollateDaysToWeeks() error {
@@ -33,7 +35,37 @@ func CollateDaysToWeeks() error {
 
 	WeekOverviewCacheInstance().Apply()
 
+	deleteOldDayDataFiles(dayDataFileNames)
+
 	return nil
+}
+
+func deleteOldDayDataFiles(fileNames []string) {
+	timeNow := time.Now()
+
+	for _, fileName := range fileNames {
+		parsedFileName, err := parseDayDataFileName(fileName)
+		if err != nil {
+			logger.LogWarning("error parsing day-data file name" +
+				"|file name=" + fileName +
+				"|error=" + err.Error())
+			continue
+		}
+
+		t := time.Date(parsedFileName.Year, time.Month(parsedFileName.Month), parsedFileName.Day, 0, 0, 0, 0, time.Local)
+		age := time_utils.GetDurationBetweenTimes(t, timeNow)
+		ageInDays := time_utils.DurationToDays(age)
+
+		if ageInDays >= app_core.MAX_DAYS_TO_KEEP_RAW_DAY_DATA_FILES {
+			err = os.Remove(app_core.PFH_DAEMON_DATA_PATH + "/" + fileName)
+			if err != nil {
+				logger.LogError("error removing stale day-data file" +
+					"|file path=" + app_core.PFH_DAEMON_DATA_PATH + "/" + fileName +
+					"|error=" + err.Error())
+				continue
+			}
+		}
+	}
 }
 
 func processDayDataFile(fileName string) error {
