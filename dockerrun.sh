9d5562c99bb2e56ef86556c0e33ae35a8a68eb43
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-29 23:53:20 +0530

    Update the dockerrun script to use bindmount

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run the docker container for pfh-reader against Local environment
 # See also: dockerbuild.sh for building the docker image locally
-docker run --restart always -d -v $HOME/.aws:/root/.aws -e app_environment=Local -e app_name=pfh_reader -e app_dir_path=/go/src/github.com/spacetimi/pfh_reader_server --publish 9000:9000 pfh-reader-server | xargs -I containerId docker logs -f containerId
+docker run --restart always -d -v $HOME/Library/Containers/com.spacetimi.pfh-daemon/Data/Documents:/root/Library/Containers/com.spacetimi.pfh-daemon/Data/Documents -e app_environment=Local -e app_name=pfh_reader -e app_dir_path=/go/src/github.com/spacetimi/pfh_reader_server -e shared_dir_path=/go/src/github.com/spacetimi/timi_shared_server --publish 9001:9001 pfh-reader-server | xargs -I containerId docker logs -f containerId
 
